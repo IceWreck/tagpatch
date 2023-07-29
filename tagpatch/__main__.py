9d5f941ef672679139f85ca19d0803d86f4964f0
Anchit Bajaj <ab@abifog.com>
2023-07-29 08:50:25 +0530

    Add patch for embedding lyrics

--- tagpatch/__main__.py
+++ tagpatch/__main__.py
@@ -3,6 +3,8 @@ import pathlib
 import tabulate
 import sys
 from tagpatch.patches import artist_name as artist_name_patch
+from tagpatch.patches import embed_lrc as embed_lrc_patch
+from tagpatch import utils
 from tagpatch import options
 
 
@@ -19,22 +21,7 @@ def cli():
 def artist_name(
     src: pathlib.Path, dst: pathlib.Path | None, assume_yes: bool, nested: bool
 ):
-    # If destination isn't provided, overwrite the source.
-    if dst is None:
-        dst = src
-
-    # Create destination if it doesn't exist.
-    if not dst.exists():
-        if dst.suffix == "":
-            dst.mkdir(parents=True, exist_ok=True)
-        else:
-            dst.touch()
-
-    # Source and destination must be of the same type.
-    if not ((src.is_dir() and dst.is_dir()) or (src.is_file() and dst.is_file())):
-        raise click.BadParameter(
-            "Source and destination must be both files or both directories."
-        )
+    src, dst = utils.prepare_src_dst(src, dst)
 
     # Display the dry run table and ask for confirmation.
     patch = artist_name_patch.ArtistNamePatch(src, dst, nested)
@@ -58,9 +45,36 @@ def artist_name(
     click.echo("Applied.")
 
 
-@cli.command()
-def embed_lrc():
-    click.echo("TODO.")
+@cli.command(help=embed_lrc_patch.EmbedLyricsPatch.help())
+@options.src
+@options.dst
+@options.assume_yes
+@options.nested
+def embed_lrc(
+    src: pathlib.Path, dst: pathlib.Path | None, assume_yes: bool, nested: bool
+):
+    src, dst = utils.prepare_src_dst(src, dst)
+
+    # Display the dry run table and ask for confirmation.
+    patch = embed_lrc_patch.EmbedLyricsPatch(src, dst, nested)
+    table = patch.prepare()
+    if len(table) == 0:
+        click.echo("No music files found in src.")
+        sys.exit(0)
+    click.echo(
+        tabulate.tabulate(
+            table,
+            headers=patch.table_headers,
+            tablefmt=patch.table_format,
+            maxcolwidths=patch.table_max_col_width,
+        )
+    )
+    if not assume_yes:
+        click.confirm("Do you want to continue?", abort=True)
+
+    # Apply the patch.
+    patch.apply()
+    click.echo("Applied.")
 
 
 def main():
---
+++ tagpatch/patches/embed_lrc.py
@@ -0,0 +1,84 @@
+import pathlib
+import shutil
+import click
+import music_tag
+import mutagen
+from tagpatch import utils
+from tagpatch.patches import patch
+from tagpatch.types import Table
+
+
+class EmbedLyricsPatch(patch.Patch):
+    _HELP_TEXT = "A patch which embeds .lrc files of the same name into the track file."
+    TAG_NAME = "lyrics"
+
+    def __init__(self, src: pathlib.Path, dst: pathlib.Path, nested: bool):
+        super().__init__()
+        self.table: Table = []
+        self.tracks = utils.get_tracks(
+            src, dst, nested
+        )  # [(absolute_src.mp3, absolute_dst.mp3), (), ...]
+
+    @classmethod
+    def help(cls) -> str:
+        return cls._HELP_TEXT
+
+    @staticmethod
+    def lrc_path(src_file: pathlib.Path) -> pathlib.Path | None:
+        """Returns the path of lrc file for the corresponding src file, if exists."""
+        if not src_file.is_file():
+            raise ValueError("src_file parameter must be a file")
+        lrc_file = src_file.with_suffix(".lrc").resolve()
+        if lrc_file.exists():
+            return lrc_file
+        return None
+
+    def prepare(self) -> Table:
+        for track in self.tracks:
+            src_file = track[0]
+            dst_file = track[1]
+            lrc_file = self.lrc_path(src_file)
+
+            colored_lrc_path = ""
+            if lrc_file is not None:
+                colored_lrc_path = utils.ansi_colorify(lrc_file)
+
+            self.table.append([colored_lrc_path, src_file, dst_file])
+        return self.table
+
+    @property
+    def table_headers(self) -> list[str]:
+        return ["Lyric File", "Source", "Destination"]
+
+    def apply(self) -> None:
+        change_log: str = "\n"
+
+        # Run with a progressbar.
+        with click.progressbar(self.tracks) as bar:
+            for track in bar:
+                src_file = track[0]
+                dst_file = track[1]
+                lrc_file = self.lrc_path(src_file)
+                
+                # If src_file is not equal to dst_file then copy src_file to dst_file first.
+                dst_file.touch()
+                if not src_file.samefile(dst_file):
+                    shutil.copy2(src_file, dst_file)
+                    change_log += f"Copied - {dst_file}\n"
+
+                # Read the .lrc file
+                modified_tag = ""
+                if lrc_file is not None:
+                    with open(lrc_file, "r") as lrcf:
+                        modified_tag = lrcf.read()
+
+                # Change tags in dst_file.
+                f: mutagen.FileType = music_tag.load_file(dst_file)
+                original_tag: str = str(f[self.TAG_NAME])
+                if original_tag != modified_tag:
+                    f[self.TAG_NAME] = modified_tag
+                    f.save()
+                    change_log += f"Patched - {dst_file}\n"
+
+        # Print the changelog.
+        click.echo(change_log)
