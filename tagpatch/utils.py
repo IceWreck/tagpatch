4edab85cb8c29ae88eff25b588ef296397209760
Anchit Bajaj <ab@abifog.com>
2023-07-29 08:09:39 +0530

    Extract src/dst checks and preparation to its own function
    Also added the ansi_colorify function.

--- tagpatch/utils.py
+++ tagpatch/utils.py
@@ -1,5 +1,6 @@
 import pathlib
 import re
+import click
 
 KNOWN_TRACK_EXTENSIONS = {".ogg", ".mp3", ".m4a", ".flac", ".opus", ".wav"}
 
@@ -46,3 +47,31 @@ def escape_ansi(line):
     """Remove ANSI color codes from text."""
     ansi_escape = re.compile(r"(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]")
     return ansi_escape.sub("", line)
+
+
+def ansi_colorify(line):
+    """Change the color of the line to red."""
+    return f"\033[31m{line}\033[0m"
+
+
+def prepare_src_dst(
+    src: pathlib.Path, dst: pathlib.Path | None
+) -> tuple[pathlib.Path, pathlib.Path]:
+    """Basic checks and preparation before using the source and destination."""
+    # If destination isn't provided, then make it equal to the source.
+    if dst is None:
+        dst = src
+    # Create destination if it doesn't exist.
+    if not dst.exists():
+        if dst.suffix == "":
+            # It is a directory.
+            dst.mkdir(parents=True, exist_ok=True)
+        else:
+            # It is a file.
+            dst.touch()
+    # Source and destination must be of the same type.
+    if not ((src.is_dir() and dst.is_dir()) or (src.is_file() and dst.is_file())):
+        raise click.BadParameter(
+            "Source and destination must be both files or both directories."
+        )
+    return src, dst
